659f02414f2ad43ff9d00946325f42e17c90c79b
ksenia-nesytova <alleheldentot@gmail.com>
2021-03-25 21:15:08 +0700

    added JS

---
+++ index.js
@@ -0,0 +1,10 @@
+let carouselItem = document.getElementsByClassName("carousel-item");
+let buttonPrev = document.getElementById("carousel-button-prev");
+let buttonNext = document.getElementById("carousel-button-next");
+
+buttonNext.addEventListener('click', function(event){
+    for(let counter = 0; counter < carouselItem.length; conter++) {
+    carouselItem[counter--].classList.remove("carousel-item-visible");
+    carouselItem[counter].className += "carousel-item-visible";
+    }
+})
