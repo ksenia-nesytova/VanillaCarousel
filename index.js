3d6df6c390ce6d07648f6d80bde82e83b6da7d2a
ksenia-nesytova <alleheldentot@gmail.com>
2021-03-25 21:19:02 +0700

    fixed carousel-items display

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let buttonNext = document.getElementById("carousel-button-next");
 
 buttonNext.addEventListener('click', function(event){
     for(let counter = 0; counter < carouselItem.length; conter++) {
-    carouselItem[counter--].classList.remove("carousel-item-visible");
+    // carouselItem[counter--].classList.remove("carousel-item-visible");
     carouselItem[counter].className += "carousel-item-visible";
     }
 })
