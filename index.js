ef89e0dd4c10f2dc229a2608c4e2bf8c3839c265
ksenia-nesytova <alleheldentot@gmail.com>
2021-03-26 14:50:04 +0700

    added functions

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let carouselItem = document.getElementsByClassName("carousel-item");
+let slide = document.getElementsByClassName("carousel-item");
 let buttonPrev = document.getElementById("carousel-button-prev");
 let buttonNext = document.getElementById("carousel-button-next");
 
-buttonNext.addEventListener('click', function(event){
-    for(let counter = 0; counter < carouselItem.length; conter++) {
-    // carouselItem[counter--].classList.remove("carousel-item-visible");
-    carouselItem[counter].className += "carousel-item-visible";
-    }
-})
+function nextSlide() {
+
+}
+
+function prevSlide() {
+
+}
+
+buttonNext.addEventListener('click', nextSlide);
+
+
+buttonPrev.addEventListener('click', prevSlide);
+
+
+// function(event){
+//    for(let counter = 0; counter < carouselItem.length; conter++) {
+//    // slide[counter--].classList.remove("carousel-item-visible");
+//    slide[counter].className += "carousel-item-visible";
+//    }
