02f6195860928c6815d881d3799185ac80d7703a
ksenia-nesytova <alleheldentot@gmail.com>
2021-03-26 15:04:23 +0700

    wrote function

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-let slide = document.getElementsByClassName("carousel-item");
+// slides
+let slides = document.getElementsByClassName("carousel-item");
+let totalSlides = slides.length;
+
+let slidePosition = 0;
+
+
+// buttons
 let buttonPrev = document.getElementById("carousel-button-prev");
 let buttonNext = document.getElementById("carousel-button-next");
 
+// functions to switch slides
 function nextSlide() {
-
+    if(slidePosition === (totalSlides-1)) {
+     slidePosition = 0;
+  } else {
+     slidePosition++;
+  }
+   slides[slidePosition].className += "carousel-item-visible";
 }
 
 function prevSlide() {
-
+   slides[slidePosition].classList.remove("carousel-item-visible");
 }
 
+
 buttonNext.addEventListener('click', nextSlide);
-
-
 buttonPrev.addEventListener('click', prevSlide);
-
-
-// function(event){
-//    for(let counter = 0; counter < carouselItem.length; conter++) {
-//    // slide[counter--].classList.remove("carousel-item-visible");
-//    slide[counter].className += "carousel-item-visible";
-//    }
